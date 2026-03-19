23907b9f90f43a795371e1a95e98a93d7b22eceb
MazaSebastian <mazasebastian@hotmail.com>
2026-03-18 22:07:51 -0300

    fix: force-dynamic rendering on shop and admin layouts for Vercel

--- src/app/(shop)/layout.tsx
+++ src/app/(shop)/layout.tsx
@@ -4,6 +4,8 @@ import { CartDrawer } from "@/components/tienda/CartDrawer";
 import Link from "next/link";
 import Image from "next/image";
 
+export const dynamic = "force-dynamic";
+
 export default function ShopLayout({
     children,
 }: {
